b34f48ec422c5f273884183a521fefc3073d5dae
Miguel Castillo <manchagnu@gmail.com>
2014-07-20 17:30:30 -0400

    Adding technical details to the read me

--- dist/module.js
+++ dist/module.js
@@ -1,37 +1,6 @@
 var Module = (function (root) {
   "use strict";
 
-  //
-  // Modules are created/registered for consumption by other modules via the define
-  // interface, and they can be either named or anonymous.  The names are the way to
-  // identify a module when it needs to be loaded as a dependency.
-  //
-  // How is define called?  Inline or remote scripts tags.  Either way, they will
-  // register possibly a name and dependencies, as well as a callback funtion to
-  // called when dependencies are resolved.
-  //
-  // Defined modules go into the pending bucket when they are named, and into the
-  // anonymous bucket when they are anonymous.  At this point, modules are not
-  // considered resolved.  Meaning, their dependency tree has not yet been traversed
-  // and loaded.  This resolution process is initiated with a require or import call.
-  // When the modules are resolved, they are moved from the pending bucket into the
-  // modules buckets, which is where all modules that have been resolved are stored.
-  //
-  // To clarify, a resolved module is a module whose dependencies have already been
-  // fully loaded and is ready to be consumed as a dependency by other modules
-  //
-  // The deferred bucket is simply a bucket of promises with the resolved module.
-  //
-  // Mdoules generally correspond to single files, but a single file could have multiple
-  // module definitions.
-  //
-  // The resolution of a module is triggered by calling either require or import.
-  // Both interfaces accomplish the same thing with just different semantics.
-  // Require is designed to be compliant with AMD/CommonJS and import is an alternative
-  // that focuses on the use of promises to deliver resolved modules. Import is also
-  // the interface for loading and resolving modules, and it is used by the require
-  // interface to load modules.
-  //
 
   ///
   /// From requirejs https://github.com/jrburke/requirejs. Thanks dude!
